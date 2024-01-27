a3fb5003ec9992e2412edf0f761e43cdfc87baf9
umeshmehta14 <ishaanmehta782@gmail.com>
2024-01-27 18:11:00 +0530

    add: delete playlist controller

--- Backend/coustom api response/src/controllers/playlist.controller.js
+++ Backend/coustom api response/src/controllers/playlist.controller.js
@@ -1,9 +1,9 @@
+import mongoose, { isValidObjectId } from "mongoose";
 import { Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-//TODO: create playlist
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
 
@@ -26,4 +26,196 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, playlist, "playlist created successfully"));
 });
 
-export { createPlaylist };
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user id");
+  }
+
+  const playlists = await Playlist.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1,
+              _id: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videos",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    "avatar.url": 1,
+                    _id: 1,
+                  },
+                },
+              ],
+            },
+          },
+
+          {
+            $project: {
+              "videoFile.url": 1,
+              "thumbnail.url": 1,
+              title: 1,
+              createdAt: 1,
+              views: 1,
+              duration: 1,
+              owner: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+  ]);
+  if (!playlists) {
+    throw new ApiError(400, "something went wrong while fetching playlists");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlists, "playlists fetched successfully"));
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist id");
+  }
+
+  const playlist = await Playlist.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(playlistId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1,
+              _id: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videos",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    "avatar.url": 1,
+                    _id: 1,
+                  },
+                },
+              ],
+            },
+          },
+
+          {
+            $project: {
+              "videoFile.url": 1,
+              "thumbnail.url": 1,
+              title: 1,
+              createdAt: 1,
+              views: 1,
+              duration: 1,
+              owner: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+  ]);
+  if (!playlist) {
+    throw new ApiError(400, "something went wrong while fetching playlist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist[0] || {}, "playlist fetched successfully")
+    );
+});
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist id");
+  }
+
+  const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId);
+
+  if (!deletedPlaylist) {
+    throw new ApiError(400, "something went wrong while deleting playlist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "playlist deleted successfully"));
+});
+
+export { createPlaylist, getUserPlaylists, getPlaylistById, deletePlaylist };
--- Backend/coustom api response/src/routes/playlist.routes.js
+++ Backend/coustom api response/src/routes/playlist.routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+  createPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  deletePlaylist,
+} from "../controllers/playlist.controller.js";
 
 const router = new Router();
 
 router.use(verifyJwt);
 
+router.route("/:playlistId").get(getPlaylistById).delete(deletePlaylist);
+// .patch(updatePlaylist)
+
 router.route("/").post(createPlaylist);
+router.route("/user/:userId").get(getUserPlaylists);
 
 export default router;
